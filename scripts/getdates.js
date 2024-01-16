79c20197628466a2faee03c4021cedd2e9daa723
Jenny <smilingjenny@gmail.com>
2024-01-16 00:09:46 -0700

    testing

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
-const now = new Date();
-const lastModified = alert(document.lastModified);
-document.getElementById('#lastModified').textContent = "Hello";
+//const now = new Date();
+//const lastModified = alert(document.lastModified);
+//document.getElementById('#lastModified').textContent = "Hello";
 
