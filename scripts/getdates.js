8e7529bbc08e767e204047fb88d873efada820af
Jenny <smilingjenny@gmail.com>
2024-01-16 00:40:55 -0700

    fixed some bugs

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 //const now = new Date();
 //const lastModified = alert(document.lastModified);
-//document.getElementById('#lastModified').textContent = "Hello";
+//document.getElementById('#lastModified').innerHTML = "Hello";
 
