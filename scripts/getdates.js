719433781e6f68dbd0c899a15a78c8223ae97de5
Jenny <smilingjenny@gmail.com>
2024-01-17 11:04:12 -0700

    Got a working js code for copy right year

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,5 @@
-// let lastModifiedDate = new Date(document.lastModified);
-// let copyRightYear = lastModifiedDate.getFullYear();
-
-//const flagIcon = <img src="images/flag-favicon.io/favicon-16x16.png"></img>
-
-// let siteAuthorInfo = `&copy ${copyRightYear} | Jennifer Walton - Utah, USA`;
-// document.getElementById("siteAuthorInfo").textContent = siteAuthorInfo;
+let copyRightYear = new Date()
+document.getElementById("copyRightYear").textContent = copyRightYear.getFullYear();
 
 
 const lastModifiedMessage = `Last Modified: ${document.lastModified}`;
