5606fa710d4a944a89e1a829cc7583c5068865af
Jenny <smilingjenny@gmail.com>
2024-01-16 20:12:33 -0700

    Last modified is working, and other paragraph is roughed in

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,12 @@
-//const now = new Date();
-//const lastModified = alert(document.lastModified);
-//document.getElementById('#lastModified').innerHTML = "Hello";
+// let lastModifiedDate = new Date(document.lastModified);
+// let copyRightYear = lastModifiedDate.getFullYear();
+
+//const flagIcon = <img src="images/flag-favicon.io/favicon-16x16.png"></img>
+
+// let siteAuthorInfo = `&copy ${copyRightYear} | Jennifer Walton - Utah, USA`;
+// document.getElementById("siteAuthorInfo").textContent = siteAuthorInfo;
+
+
+const lastModifiedMessage = `Last Modified: ${document.lastModified}`;
+document.getElementById('lastModified').textContent = lastModifiedMessage;
 
