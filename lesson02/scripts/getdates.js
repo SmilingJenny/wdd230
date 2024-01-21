8c124d36e01d2c982b0e5425cc5b1e519bde6d13
Jenny <smilingjenny@gmail.com>
2024-01-20 19:22:40 -0700

    First pass at design project

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,7 @@
+let copyRightYear = new Date()
+document.getElementById("copyRightYear").textContent = copyRightYear.getFullYear();
+
+
+const lastModifiedMessage = `Last Modified: ${document.lastModified}`;
+document.getElementById('lastModified').textContent = lastModifiedMessage;
+
