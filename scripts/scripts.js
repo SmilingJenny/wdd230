9b6337757779d477e0f4355e0828756763e21173
Jenny <smilingjenny@gmail.com>
2024-01-26 22:09:33 -0700

    added map and hamburger menu

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+const hamButton = document.querySelector("#hamButton")
+const mainNavBar = document.querySelector("#mainNavBar")
+
+hamButton.addEventListener("click", () => {
+    hamButton.classList.toggle("show");
+    mainNavBar.classList.toggle("show");
+});
