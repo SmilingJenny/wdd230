c2b4d15289c8265ee6e8b8bcfe355df2407bff53
Jenny <smilingjenny@gmail.com>
2024-02-07 23:58:15 -0700

    fixed bugs

--- lesson05/scripts/scripts.js
+++ lesson05/scripts/scripts.js
@@ -3,26 +3,28 @@ const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
 button.addEventListener("click", () => {
-    if (input.value !== "") {
-    } else {
+    if (input.value != "") {
+        const li = document.createElement('li');
+        const delButton = document.createElement('button');
+        delButton.setAttribute("aria-label", "delete scripture entry")
+    
+        li.textContent = input.value;
+        delButton.textContent = "❌";
+        li.append(delButton);
+        list.appendChild(li);
+    
+        delButton.addEventListener("click", () => {
+            list.removeChild(li);
+            input.focus();
+        });
+    
         input.focus();
+        input.value = "";
+
+    } else {
+        input.setAttribute("placeholder", "Enter a book and chapter"); 
     }
 
-    const li = document.createElement('li');
-    const delButton = document.createElement('button');
-    delButton.setAttribute("aria-label", "delete scripture entry")
-
-    li.textContent = input.value;
-    delButton.textContent = "❌";
-    li.append(delButton);
-    list.appendChild(li);
-
-    delButton.addEventListener("click", () => {
-        list.removeChild(li);
-        input.focus();
-    });
-
-    input.focus();
-    input.value = "";
+    
 
 });
