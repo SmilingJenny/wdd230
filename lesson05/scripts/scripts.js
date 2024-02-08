81e57f6ec227928f2d14dbf10887693ef4317951
Jenny <smilingjenny@gmail.com>
2024-02-08 00:06:17 -0700

    fixed bug

--- lesson05/scripts/scripts.js
+++ lesson05/scripts/scripts.js
@@ -2,29 +2,32 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
+input.focus();
+
 button.addEventListener("click", () => {
     if (input.value != "") {
         const li = document.createElement('li');
         const delButton = document.createElement('button');
         delButton.setAttribute("aria-label", "delete scripture entry")
-    
+
         li.textContent = input.value;
         delButton.textContent = "❌";
         li.append(delButton);
         list.appendChild(li);
-    
+
         delButton.addEventListener("click", () => {
             list.removeChild(li);
             input.focus();
         });
-    
+
         input.focus();
         input.value = "";
 
     } else {
-        input.setAttribute("placeholder", "Enter a book and chapter"); 
+        input.setAttribute("placeholder", "Enter a book and chapter");
+        input.focus();
     }
 
-    
+
 
 });
