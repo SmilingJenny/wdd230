b4eb128d057f021a9a0dbce8ab3375fcca38fd89
Jenny <smilingjenny@gmail.com>
2024-02-08 14:39:41 -0700

    polishing

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -3,8 +3,23 @@ const heroImage = document.querySelector("#heroImage")
 const main = document.querySelector("main")
 const modeButton = document.querySelector("#modeButton")
 
+const logo = document.querySelector("#logo");
+
 modeButton.addEventListener("click", () => {
     header.classList.toggle("darkMode");
+    modeButton.classList.toggle("darkMode");
     heroImage.classList.toggle("darkMode");
     main.classList.toggle("darkMode");
+
+    console.log(logo.src)
+
+    if (logo.src.endsWith("images/cropped-provo-logo.png")) {
+        logo.src = "images/cropped-white-logo.png";
+    } else {
+        logo.src = "images/cropped-provo-logo.png"
+    }
 });
+
+
+
+
