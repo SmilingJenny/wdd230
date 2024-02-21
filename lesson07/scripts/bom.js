63e75a70f2397d2c2fdf63367707644c373aecd9
Jenny <smilingjenny@gmail.com>
2024-02-20 18:56:41 -0700

    bom setup

---
+++ lesson07/scripts/bom.js
@@ -0,0 +1,35 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+const chaptersArray = []
+// const getChaptersList = chaptersArray
+
+input.focus();
+
+button.addEventListener("click", () => {
+    if (input.value != "") {
+        const li = document.createElement('li');
+        const delButton = document.createElement('button');
+        delButton.setAttribute("aria-label", "delete scripture entry")
+
+        li.textContent = input.value;
+        delButton.textContent = "❌";
+        li.append(delButton);
+        list.appendChild(li);
+
+        delButton.addEventListener("click", () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.focus();
+        input.value = "";
+
+    } else {
+        input.setAttribute("placeholder", "Enter a book and chapter");
+        input.focus();
+    }
+
+
+
+});
