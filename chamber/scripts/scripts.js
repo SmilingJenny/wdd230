2724616d4114c77077b98185049d1d870a63a002
Jenny <smilingjenny@gmail.com>
2024-02-22 15:22:35 -0700

    finished dates between visits

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -6,3 +6,19 @@ hamButton.addEventListener("click", () => {
     mainNavBar.classList.toggle("show");
 });
 
+const visitsDisplay = document.querySelector(".visits");
+const date = new Date();
+
+function timeBetweenVisits(date) {
+    if (localStorage.getItem("visit") !== "") {
+        const today = date
+        const lastDay = localStorage.getItem("visit")
+        const dayDiff = date - lastDay
+} else {
+    visitsDisplay.textContent = "Welcome! Let us know if you have any questions."
+    localStorage.setItem("visit", JSON.stringify(date))
+}
+
+}
+
+console.log()
