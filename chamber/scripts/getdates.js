d576250acfd2502a8cdc9a02cd4a76f6ba811eb3
Jenny <smilingjenny@gmail.com>
2024-03-01 14:08:27 -0700

    hidden date, table work and membership radio work

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -5,3 +5,9 @@ document.getElementById("copyRightYear").textContent = copyRightYear.getFullYear
 const lastModifiedMessage = `Last Modified: ${document.lastModified}`;
 document.getElementById('lastModified').textContent = lastModifiedMessage;
 
+// form hidden date
+const hiddenDate = document.querySelector("#date")
+const dateNow = Date.now()
+document.querySelector("#date").value = dateNow
+
+
