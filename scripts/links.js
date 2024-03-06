2f44f63f08b86e9dcf320e6e18787f3824d64149
Jenny <smilingjenny@gmail.com>
2024-03-06 13:42:58 -0700

    json

---
+++ scripts/links.js
@@ -0,0 +1,26 @@
+const baseURL = "https://smilingjenny.github.io/wdd230/"
+const linksURL = "https://smilingjenny.github.io/wdd230/data/links.json"
+const learningLinksList = document.querySelector("#learning-links")
+
+async function getLinks() {
+    const response = await fetch(linksURL)
+    const data = await response.json()
+    // console.log(data)
+    displayLinks(data.lessons)
+}
+
+getLinks()
+
+const displayLinks = (weeks) => {
+    weeks.forEach(week => {
+        let link = document.createElement("li")
+        let anchor = document.createElement("a")
+
+        anchor.setAttribute("href", week.links.url)
+        anchor.setAttribute("target", "_blank")
+        anchor.textContent = `${week.lesson} - ${week.links.title}`
+        // console.log(anchor)
+        link.innerHTML = anchor
+        learningLinksList.appendChild(link)
+    })
+}
