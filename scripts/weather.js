0fe0f120d87893428fe619f22557ac63d0f061ab
Jenny <smilingjenny@gmail.com>
2024-03-15 18:54:41 -0600

    fixed portal figure too

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,8 @@
 const temp = document.getElementById("curr-temp") 
 const humidity = document.getElementById("humidity")
-const icon = document.getElementById("weather-icon")
-const description = document.getElementById("description")
+const iconFig = document.getElementById("iconFig")
+// const icon = document.getElementById("weather-icon")
+// const description = document.getElementById("description")
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=40.23&lon=-111.65&appid=b865574f47333eba1210cb2956e0e422&units=imperial"
 
 async function fetchWeather() {
@@ -23,15 +24,19 @@ fetchWeather()
 function displayResults(data) {
     const tempRounded = Math.round(data.main.temp)
     const humRounded = Math.round(data.main.humidity)
-    const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-    let desc = data.weather[0].description
-    let capDesc = titleCase(desc)
+    const iconImg = document.createElement("img")
+    const iconImgSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    const iconCaption = document.createElement("figcaption")
+    const desc = data.weather[0].description
+    const capDesc = titleCase(desc)
 
     temp.innerHTML = `${tempRounded}&deg;F`
     humidity.innerHTML = `${humRounded}&deg;F`
-    icon.setAttribute("src", iconSrc)
-    icon.setAttribute("alt", "Weather Icon")
-    description.innerHTML = `Current Weather Conditions: <br>${capDesc}`   
+    iconImg.setAttribute("src", iconImgSrc)
+    iconImg.setAttribute("alt", "Weather Icon")
+    iconFig.appendChild(iconImg)
+    iconCaption.innerHTML = `Current Weather Condition: <br>${capDesc}`
+    iconFig.appendChild(iconCaption)   
 }
 
 function titleCase(desc) {
