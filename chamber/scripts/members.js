e0cac2beb917981004180a852e65235bd1898370
Jenny <smilingjenny@gmail.com>
2024-03-16 22:10:14 -0600

    removed comment

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -51,30 +51,3 @@ const displayMembers = (members) => {
         membersDiv.appendChild(section)
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-// const section = document.createElement("section")
-// const logo = document.createElement("img")
-// const infoPara = document.createElement("p")
-// const memberInfo = [member.name, member.address, member.phoneNumber, member.websiteURL, member.membershipLevel, member.membershipYears, member.industry]
-
-// section.setAttribute("class", "card")
-// logo.setAttribute("src", member.logoFile)
-// logo.setAttribute("alt", `${member.name} Logo`)
-// infoPara.innerHTML = memberInfo.join("<br>")
-// console.log(infoPara)
-
-// section.appendChild(logo)
-// section.appendChild(infoPara)
-// membersDiv.appendChild(section)
-
-// console.log(membersDiv)
