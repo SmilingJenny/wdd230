c341d139bce45d823cb96a1943af57a183186f52
Jenny <smilingjenny@gmail.com>
2024-03-19 12:58:44 -0600

    cleaned up darkmode js

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,19 +1,17 @@
 const body = document.querySelector("body")
 const header = document.querySelector("header")
+const meetingBanner = document.querySelector("#meetingBanner")
 const heroImage = document.querySelector("#heroImage")
 const main = document.querySelector("main")
-const modeButton = document.querySelector("#modeButton")
-const meetingBanner = document.querySelector("#meetingBanner")
-const logo = document.querySelector("#logo");
 const footer = document.querySelector("footer");
+const modeButton = document.querySelector("#modeButton")
 
 modeButton.addEventListener("click", () => {
     body.classList.toggle("darkMode");
     header.classList.toggle("darkMode");
-    // modeButton.classList.toggle("darkMode");
+    meetingBanner.classList.toggle("darkMode");
     heroImage.classList.toggle("darkMode");
     main.classList.toggle("darkMode");
-    meetingBanner.classList.toggle("darkMode");
     footer.classList.toggle("darkMode");
     
 
