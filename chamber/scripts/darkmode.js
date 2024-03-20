61ad39c478a9e905238db47384bd0d782bc9755e
Jenny <smilingjenny@gmail.com>
2024-03-20 16:31:49 -0600

    finished adding dark mode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,19 +1,7 @@
 const body = document.querySelector("body")
-const header = document.querySelector("header")
-const meetingBanner = document.querySelector("#meetingBanner")
-const heroImage = document.querySelector("#heroImage")
-const main = document.querySelector("main")
-const footer = document.querySelector("footer");
-const modeButton = document.querySelector("#modeButton")
 
 modeButton.addEventListener("click", () => {
     body.classList.toggle("darkMode");
-    header.classList.toggle("darkMode");
-    meetingBanner.classList.toggle("darkMode");
-    heroImage.classList.toggle("darkMode");
-    main.classList.toggle("darkMode");
-    footer.classList.toggle("darkMode");
-    
 
     if (logo.src.endsWith("images/webp-blue-logo.webp")) {
         logo.src = "images/webp-white-logo.webp";
