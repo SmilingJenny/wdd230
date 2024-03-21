4dc718053e6a938674debf52221c53c6d58a9003
Jenny <smilingjenny@gmail.com>
2024-03-21 11:05:16 -0600

    fixed banner js function typo

--- chamber/scripts/meeting-banner.js
+++ chamber/scripts/meeting-banner.js
@@ -1,10 +1,10 @@
-const meetingBanBtn = document.querySelector("#meetingBanBtn")
 const meetingBanner = document.querySelector("#meetingBanner")
+const meetingBanBtn = document.querySelector("#meetingBanBtn")
 
 function meetingBannerDisplay() {
-    const day = new Date().getDay
-    
-    if (day >=1 && day <=3) {
+    const day = new Date().getDay()
+
+    if (day >= 1 && day <= 3) {
         meetingBanner.classList.toggle("show")
     } else {
         meetingBanner.classList.toggle("close")
@@ -14,4 +14,5 @@ meetingBannerDisplay()
 
 meetingBanBtn.addEventListener("click", () => {
     meetingBanner.classList.toggle("close")
+    meetingBanner.classList.toggle("show")
 });
