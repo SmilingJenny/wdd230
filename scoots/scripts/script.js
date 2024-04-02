d79bef395f0a5b78881c85cb9f372d3640ce3888
Jenny <smilingjenny@gmail.com>
2024-04-01 22:55:54 -0600

    made placeholder hi-temp. banner with working close button

--- scoots/scripts/script.js
+++ scoots/scripts/script.js
@@ -1,5 +1,14 @@
-const hamBtn = document.querySelector("#hamBtn")
-const navBar = document.querySelector("#navBar")
+const tempBtn = document.getElementById("tempBtn")
+const tempBan = document.getElementById("tempBan")
+
+const hamBtn = document.getElementById("hamBtn")
+const navBar = document.getElementById("navBar")
+
+tempBan.classList.add("open")
+
+tempBtn.addEventListener("click", () => {
+    tempBan.classList.remove("open")
+})
 
 hamBtn.addEventListener("click", () => {
     hamBtn.classList.toggle("show")
